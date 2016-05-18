2f9be2cda49e2bfb335f00d59d61b449973f2e1b
unknown <bterkaly@BRUNOLENOVO.redmond.corp.microsoft.com>
2016-05-18 17:25:03 -0500

    first commit

--- checkin.bat
+++
@@ -1,8 +0,0 @@
-git init
-git add * --all
-$ git config --global core.excludesfile checkin.bat
-git rm checkin.bat
-git commit -m "commit"
-git remote add origin https://github.com/brunoterkaly/java-loves-data.git
-git push -u origin master
-
